9fda6e00705028d63c5920f2d267f1eb27b85e05
Dominik Kundel <dominik.kundel@gmail.com>
2018-04-08 18:25:06 +0200

    :pencil2: Add more info to contributors section

--- generators/contributors/index.js
+++ generators/contributors/index.js
@@ -4,9 +4,13 @@ const extend = require('lodash.defaultsdeep');
 const { readPkgForGenerator, writePkgForGenerator } = require('../utils');
 
 const CONTRIBUTORS_README = stripIndent`
+  Thanks goes to these wonderful people ([emoji key](https://github.com/kentcdodds/all-contributors#emoji-key)):
+
   <!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
 
   <!-- ALL-CONTRIBUTORS-LIST:END -->
+
+  This project follows the [all-contributors](https://github.com/kentcdodds/all-contributors) specification. Contributions of any kind welcome!
 `;
 
 module.exports = class extends Generator {
